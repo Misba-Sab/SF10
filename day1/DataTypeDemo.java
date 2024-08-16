7adefae10fee71c7cc27fc6892210c7b30efa9c7
Misba-Sab <misbaksab@gmail.com>
2024-08-16 18:15:36 +0530

    Add files via upload

---
+++ day1/DataTypeDemo.java
@@ -0,0 +1,16 @@
+package com.rnsit.day1;
+
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int value1 = 9/2;
+        float value2 = 101f/61f;
+        double value3 = 10d/6d;
+        float value4 = 5/2.5f;
+
+        System.out.println("Value 1 = "+value1);
+        System.out.println("Value 2 = "+value2);
+        System.out.println("Value 3 = "+value3);
+        System.out.println("Value 4 = "+value4);
+
+    }
+}
---
+++ day1/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.rnsit.day1;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello RNSIT Students");
+        System.out.println("C2TC program");
+        System.out.print("SF10 students");
+        System.out.println("Day 1 of class");
+    }
+}
